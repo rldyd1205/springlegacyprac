5beff076faad4db473e5ebf1b581dc88fd5e84b1
KGY <rldyd1205@naver.com>
2021-12-10 16:41:17 +0900

    commit

--- springlegacy/src/main/java/com/example/controller/HomeController.java
+++ springlegacy/src/main/java/com/example/controller/HomeController.java
@@ -1,39 +1,17 @@
 package com.example.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	// index화면 보여주기
+	@GetMapping(value = {"/","/index"})
+	public String home() {
+		System.out.println("home 호출됨...");
+		return "index";
 	}
-	
 }
