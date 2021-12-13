787b6867178896f1542bcfcf29957e2071359b22
KGY <rldyd1205@naver.com>
2021-12-13 16:27:06 +0900

    commmit

--- springlegacy/src/main/java/com/example/service/MemberService.java
+++ springlegacy/src/main/java/com/example/service/MemberService.java
@@ -6,6 +6,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.domain.MemberVO;
 import com.example.mapper.MemberMapper;
 
+//MemberMapper는 interface라서 private로 가져 와야 한다.
+//즉 MemberMapper와 MemberService스프링에게 말해서 연결시키는 코드
+
+//그래서 사용하려면 생성자가 필요함
+
 @Service
 @Transactional
 public class MemberService {
