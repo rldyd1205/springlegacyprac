66c9942fa370bc62b1d0ca18b0775a7e40b6e616
KGY <rldyd1205@naver.com>
2022-01-03 23:07:44 +0900

    commit

--- springlegacy/src/main/java/com/example/interceptor/loginCheckInterceptor.java
+++ springlegacy/src/main/java/com/example/interceptor/loginCheckInterceptor.java
@@ -1,33 +1,41 @@
 package com.example.interceptor;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.example.domain.MemberVO;
 import com.example.service.MemberService;
 
-public class loginCheckInterceptor implements HandlerInterceptor{
-	
+public class loginCheckInterceptor implements HandlerInterceptor {
+
 	private MemberService memberService;
 
 	public loginCheckInterceptor(MemberService memberService) {
 		super();
 		this.memberService = memberService;
 	}
-	
+
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-		
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws IOException {
+
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("id");
-		
+
 		if (id == null) {
 			response.sendRedirect("/member/login");
-			
+
 			return false;
 		}
+
+		memberService.getMemberById(id);
+		System.out.println(memberVO.toString());
+
 		return true;
 	}
 }
