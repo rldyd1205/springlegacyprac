0b3bde579bd0ed126e326b34b008f26f3fdd7ea4
KGY <rldyd1205@naver.com>
2022-01-03 23:46:29 +0900

    commit

--- springlegacy/src/main/java/com/example/interceptor/RememberMeInterceptor.java
+++ springlegacy/src/main/java/com/example/interceptor/RememberMeInterceptor.java
@@ -6,34 +6,50 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 // 로그인 유지
-public class RememberMeInterceptor implements HandlerInterceptor{
-	
+public class RememberMeInterceptor implements HandlerInterceptor {
+
 	// 세션이 없을 때 쿠키를 뒤져봐서 있으면 그걸로 로그인
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-		
+
 		// 세션이 있는지 확인
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("id");
-		
+
 		// 세션 없으면 쿠키확인
 		if (id == null) {
 			Cookie[] cookies = request.getCookies();
-			
+
 			if (cookies == null) {
 				for (Cookie cookie : cookies) {
 					if (cookie.getName().equals("UserId")) {
 						id = cookie.getValue();
-						
+
 						session.setAttribute("id", id);
 					}
-				}// for
-			} 
-		}// if
-		
+				} // for
+			}
+		} // if
+
+		// 리턴값이 true면 호출이 예정되어있는 해당 컨트롤러 메소드를 호출함.
+		// 리턴값이 false면 호출이 예정되어있는 해당 컨트롤러 메소드를 호출 안함.
 		return true;
 	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// 컨트롤러 메소드 실행 직후 view페이지 렌더링 되기 전 호출됨
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// view렌더링 후 호출됨
+	}
+
 	
 }
