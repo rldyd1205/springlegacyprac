60da49b39851657dfb2d0ce222f5665bb21d82a6
KGY <rldyd1205@naver.com>
2022-01-03 23:39:39 +0900

    commit

--- springlegacy/src/main/java/com/example/interceptor/RememberMeInterceptor.java
+++ springlegacy/src/main/java/com/example/interceptor/RememberMeInterceptor.java
@@ -7,14 +7,18 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+// 로그인 유지
 public class RememberMeInterceptor implements HandlerInterceptor{
 	
+	// 세션이 없을 때 쿠키를 뒤져봐서 있으면 그걸로 로그인
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		
+		// 세션이 있는지 확인
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("id");
 		
+		// 세션 없으면 쿠키확인
 		if (id == null) {
 			Cookie[] cookies = request.getCookies();
 			
