2aae38c98970663da8b3d414b0681be574aeb445
KGY <rldyd1205@naver.com>
2022-01-03 23:04:00 +0900

    commit

--- springlegacy/src/main/java/com/example/interceptor/loginCheckInterceptor.java
+++ springlegacy/src/main/java/com/example/interceptor/loginCheckInterceptor.java
@@ -2,6 +2,7 @@ package com.example.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -19,6 +20,12 @@ public class loginCheckInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		
+		if (id == null) {
+			response.sendRedirect("/member/login");
+		}
 		return true;
 	}
 }
