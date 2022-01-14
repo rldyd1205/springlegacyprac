62e06624206c8099a9f1e21343b8a0aad4151573
KGY <rldyd1205@naver.com>
2022-01-14 13:16:44 +0900

    commit

--- springlegacy/src/main/java/com/example/controller/BoardController.java
+++ springlegacy/src/main/java/com/example/controller/BoardController.java
@@ -1,8 +1,13 @@
 package com.example.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.domain.AttachVO;
@@ -20,9 +26,13 @@ import com.example.domain.Criteria;
 import com.example.domain.PageDTO;
 import com.example.service.BoardService;
 
+import net.coobird.thumbnailator.Thumbnailator;
+
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
+
+	private final String BASE_PATH = "C:/uploadFolder/upload";
 
 	private BoardService boardService;
 
@@ -58,17 +68,100 @@ public class BoardController {
 		return "board/writeBoard";
 	}
 
+	// "년/월/일' 형식의 폴더명을 리턴하는 메소드
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+		String str = sdf.format(new Date());
+
+		return str;
+	}// getFolder
+
+	// 이미지 파일인지 체크
+	private boolean checkImageType(File file) throws IOException {
+		boolean isImage = false;
+
+		String contentType = Files.probeContentType(file.toPath()); // "image/jpg" "image/png" 등으로 리턴함.
+		System.out.println("contentType : " + contentType);
+
+		isImage = contentType.startsWith("image"); // image로 시작할 때 true로 리턴
+
+		return isImage;
+	} // checkImageType
+
+	private List<AttachVO> uploadFilesAndGetAttachList(List<MultipartFile> files, int boardNum)
+			throws IllegalStateException, IOException {
+
+		List<AttachVO> attachList = new ArrayList<AttachVO>();
+
+		// null 체크
+		if (files == null || files.size() == 0) {
+			System.out.println("첨부파일 없음 ...");
+			return attachList;
+		}
+
+		File uploadPath = new File(BASE_PATH, getFolder());
+		// getFolder() -> "yyyy/MM/dd" 형식의 String 으로 리턴
+		// uploadPath = "C:/uploadFile/upload/2021/12/29"
+
+		// 폴더 경로가 존재 하는지 체크
+		if (uploadPath.exists() == false) { // 경로가 존재하지 않을때
+			uploadPath.mkdirs(); // 경로를 생성해줌
+		}
+
+		// files 에 있는 파일 하나씩 꺼내서 처리하기
+		for (MultipartFile multipartFile : files) {
+			if (multipartFile.isEmpty()) { // 파일이 비어있으면 다음 파일로 넘어가기
+				continue;
+			}
+
+			String originalFilename = multipartFile.getOriginalFilename();
+
+			UUID uuid = UUID.randomUUID();
+			String uploadFilename = uuid.toString() + "_" + originalFilename;
+
+			File file = new File(uploadPath, uploadFilename);
+			// file = "C:/uploadFile/upload/2021/12/29" + "/" +
+			// "ajskdfo2kgixldfijkwle_a.jpg"
+
+			multipartFile.transferTo(file); // 경로에 맞게 파일 생성
+
+			// 첨부파일이 이미지일 경우
+			boolean isImage = checkImageType(file);
+			if (isImage == true) { // 이미지 파일일 때 파일이름에 s_ 붙여서 썸네일 파일 추가로 만들기
+				File outFile = new File(uploadPath, "s_" + uploadFilename);
+
+				// "C:/uploadFile/upload/2021/12/29" + "s_ajskdfo2kgixldfijkwle_a.jpg"
+
+				Thumbnailator.createThumbnail(file, outFile, 100, 100);
+			}
+
+			AttachVO attachVO = new AttachVO();
+			attachVO.setUuid(uuid.toString());
+			attachVO.setUploadpath(getFolder());
+			attachVO.setFilename(originalFilename);
+			attachVO.setFiletype(isImage ? "I" : "O");
+			attachVO.setBoardNum(boardNum);
+
+			attachList.add(attachVO);
+
+		} // for
+
+		return attachList;
+	} // uploadFilesAndGetAttachList
+
 	// 새글 쓰기 버튼 눌렀을 때 기능 구현
 	// 기능 다 작동하고 나서 return으로 board/boardContent페이지 창 띠어주기
 	// board/boardContent -> 게시글 상세보기
 	@PostMapping("write")
-	public String write(BoardVO boardVO, HttpSession session, RedirectAttributes rttr) {
+	public String write(List<MultipartFile> files, BoardVO boardVO, HttpSession session, RedirectAttributes rttr) throws IllegalStateException, IOException {
 		System.out.println("수정 전 BoardVO : " + boardVO);
 
 		// 1. 새 글 번호 추가
 		int nextNum = boardService.getNextNum();
-		System.out.println("nextNum : " + nextNum);
 		boardVO.setNum(nextNum);
+
+		List<AttachVO> attachList = uploadFilesAndGetAttachList(files, nextNum);
 		
 		// 2. 글쓴이 추가
 		String id = (String) session.getAttribute("id");
