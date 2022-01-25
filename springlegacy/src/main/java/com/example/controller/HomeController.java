6be3f54d24688cd21b1a73e337311085f3534259
KGY <rldyd1205@naver.com>
2022-01-25 16:59:30 +0900

    commit

--- springlegacy/src/main/java/com/example/controller/HomeController.java
+++ springlegacy/src/main/java/com/example/controller/HomeController.java
@@ -1,7 +1,20 @@
 package com.example.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -9,9 +22,80 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	// index화면 보여주기
-	@GetMapping(value = {"/","/index"})
+	@GetMapping(value = { "/", "/index" })
 	public String home() {
 		System.out.println("home 호출됨...");
 		return "index";
 	}
+
+	// /display?fileName=xxxxxxxxxxxxx
+	@GetMapping("/display")
+	@ResponseBody // 컨트롤러 메소드가 리턴하는 데이터 자체를 바로 응답으로 주고자 할 경우 사용
+	public ResponseEntity<byte[]> getImageFile(String fileName) throws IOException {
+		// byte배열 객체로 보내기
+		System.out.println("fileName : " + fileName);
+
+		File file = new File("C:/uploadFolder/upload", fileName);
+		// "C:/uploadFolder/upload" + "yy/MM/dd/dog.jpg"
+		System.out.println("실제 이미지 파일 경로 : " + file.getPath());
+
+		HttpHeaders headers = new HttpHeaders();
+
+		String contentType = Files.probeContentType(file.toPath());
+
+		headers.add("Content-Type", contentType);
+
+		// 이미지 파일을 byte배열로 변환
+		byte[] imageData = FileCopyUtils.copyToByteArray(file);
+
+		// responseEntity(보낼 body데이터, headers, 상태)
+		return new ResponseEntity<byte[]>(imageData, headers, HttpStatus.OK);
+	} // getImageFile
+
+	// /download?fileName=xxxxxxxxx
+	// 속성 여러개 넣으려면. 경로값에 value로 하기(원래 생략했던것)
+	// produces : contentType가 없을 때 지정할 application/octet-stream으로 설정하기
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> download(String fileName) throws UnsupportedEncodingException {
+		// 스트링이랑 연동이 잘되는 Resource 타입으로 보내기
+		System.out.println("fileName : " + fileName);
+
+		File file = new File("C:/uploadFolder/upload", fileName);
+		// "C:/uploadFolder/upload" + "yy/MM/dd/dog.jpg"
+
+		// 파일정보 resource
+		Resource resource = new FileSystemResource(file);
+		System.out.println("resource : " + resource);
+
+		if (resource.exists() == false) { // 다운로드할 파일이 존재하지 않을때
+			// 404코드, 자원없음 응답코드 보내고 종료.
+			return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+		}
+
+		// 다운로드 할 파일(resource)이 존재하면
+
+		String resourceName = resource.getFilename();
+
+		System.out.println("resourceName : " + resourceName);
+		// resourceName : 1c228983-d76e-46dd-a427-897bfafc6e7a_dog.jpg
+		// 앞에 uuid 지저분하기 때문에 잘라내기
+		int beginIndex = resourceName.indexOf("_") + 1;
+		String originFilename = resourceName.substring(beginIndex);
+		System.out.println("originFilename : " + originFilename); // dog.jpg
+
+		// 한글 파일명이면 한글 깨지는거 해결
+		// 다운로드 파일명의 문자셋을 utf-8에서 iso-8859-1로 변경
+		// 기존문자열을 바이트배열로 바꿔서 iso-8859-1의 문자열로 바꿔줌
+		String downloadName = new String(originFilename.getBytes("utf-8"), "iso-8859-1");
+		System.out.println("downloadName : " + downloadName);
+
+		HttpHeaders headers = new HttpHeaders();
+		// content-type은 애노테이션의 produces속성으로 처리함
+		// headers.add("Content-Type", "application/octet-stream");
+		headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	} // download
+
 }
--- springlegacy/src/main/webapp/WEB-INF/views/board/boardContent.jsp
+++ springlegacy/src/main/webapp/WEB-INF/views/board/boardContent.jsp
@@ -1,7 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt"  prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +16,7 @@
 	margin-top: 30px;
 }
 
-.rows{
+.rows {
 	border-bottom: black solid 1px;
 }
 
@@ -27,11 +28,12 @@
 	font-size: 30px;
 }
 
-#row3{
+#row3 {
 	padding: 20px;
 	font-size: 25px;
 	height: 450px;
 }
+
 #regDate {
 	font-size: 20px;
 }
@@ -56,20 +58,21 @@
 				</div>
 				<div class="divider-custom-line"></div>
 			</div>
-			<div >
+			<div>
 				<button type="button" class="btn btn-primary"
-				onclick="location.href='/board/list?pageNum=${ pageNum }'">게시글 목록으로 돌아가기</button>
+					onclick="location.href='/board/list?pageNum=${ pageNum }'">게시글
+					목록으로 돌아가기</button>
 			</div>
-			
+
 			<!-- Contact Section Form-->
 			<div class="row justify-content-center" id="content-box">
 				<div class="row rows" id="row1">
 					<div id="boardNum" class="col-lg-9">
-						<span>${ board.num }</span>&nbsp;&nbsp;&nbsp;
-						<span>${ board.subject }</span>
+						<span>${ board.num }</span>&nbsp;&nbsp;&nbsp; <span>${ board.subject }</span>
 					</div>
 					<div id="regDate" class="col-lg-3">
-						<span>등록날짜 <fmt:formatDate value="${ board.regDate }" pattern="yyyy.MM.dd HH:mm"/></span>
+						<span>등록날짜 <fmt:formatDate value="${ board.regDate }"
+								pattern="yyyy.MM.dd HH:mm" /></span>
 					</div>
 				</div>
 				<div class="row rows" id="row2">
@@ -79,9 +82,40 @@
 					<div id="viewCount" class="col-lg-2">
 						<span>조회수 ${ board.viewCount }</span>
 					</div>
-				</div>	
-				<div class="row rows" id="row3">
-					${ board.content }
+				</div>
+				<div class="row rows" id="row3">${ board.content }</div>
+				<div class="row rows">
+					<h3>첨부파일</h3>
+					<c:choose>
+						<c:when test="${ fn:length(board.attachList) gt 0 }">
+							<c:forEach var="attach" items="${ board.attachList }">
+
+								<c:if test="${ attach.filetype eq 'O' }">
+									<!-- 일반파일 -->
+									<!-- 다운로드할 일반파일 경로 변수 만들기 pageScope로 저장 -->
+									<c:set var="fileCallPath"
+										value="${ attach.uploadpath }/${ attach.uuid }_${ attach.filename }" />
+									<li><a href="/download?fileName=${ fileCallPath }"> 💾
+											${ attach.filename } </a></li>
+								</c:if>
+								<c:if test="${ attach.filetype eq 'I' }">
+									<!-- 이미지파일 -->
+									<c:set var="fileCallPath"
+										value="${ attach.uploadpath }/s_${ attach.uuid }_${ attach.filename }" />
+									<c:set var="fileCallPathOrigin"
+										value="${ attach.uploadpath }/${ attach.uuid }_${ attach.filename }" />
+									<li><a href="/download?fileName=${ fileCallPathOrigin }">
+											💾 ${ attach.filename }<img
+											src="/display?fileName=${ fileCallPath }"
+											style="width: 200px">
+									</a></li>
+								</c:if>
+							</c:forEach>
+						</c:when>
+						<c:otherwise>
+							첨부파일 없음
+						</c:otherwise>
+					</c:choose>
 				</div>
 			</div>
 		</div>
